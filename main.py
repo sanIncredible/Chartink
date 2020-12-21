842e391452a167a8b5a9b25c32ca08b1a862bb4b
santosh <sky.nitc@gmail.com>
2020-12-21 23:09:28 +0530

    Update main.py
    Del bot token

--- main.py
+++ main.py
@@ -41,14 +41,14 @@ def telegram_bot_sendtext(bot_message,title):
 
     # bot_message=bot_message.replace('&','-')
     # bot_message= datetime.now().strftime("%m/%d/%Y, %H:%M:%S").__str__() +"\n\n"+ title + "\n\n" + bot_message
-    # bot_token='1323516991:AAEmL6Yd4Z8GW990YOsRTqwJT0hx-wawBYo'
+    # '
     # bot_chatID='@NimblrORB'
     # send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     # response=requests.get(send_text)
 
 
 def telegram_bot_send_image(image):
-    url = "https://api.telegram.org/bot1323516991:AAEmL6Yd4Z8GW990YOsRTqwJT0hx-wawBYo/sendPhoto";
+    url = "";
     files = {'photo': open(image, 'rb')}
     data = {'chat_id': "@NimblrORB"}
     r = requests.post(url, files=files, data=data)
